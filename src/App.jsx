12ae1d7d5c5d8d62735e8de202bf5c04723cf670
Nischal Subedi <nischal1337@gmail.com>
2024-07-15 14:43:58 +0545

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react"
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div> <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1></div>
-  )
-}
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection: bg-cyan-300 selection:text-cyan-900'>
+      <div className="flixed top-0 -z-10 h-full w-full"></div >
 
-export default App
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] 
+      bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+      <div className="container mx-auto px-8">    <Navbar /></div>
+
+    </div>
+  );
+};
+
+export default App;
