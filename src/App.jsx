d11a0b79e4279fb1942a98ad524ea877e8442c93
Nischal Subedi <nischal1337@gmail.com>
2024-07-18 20:09:57 +0545

    edit: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from './components/Navbar'
 
+
 const App = () => {
   return (
     <div className='overflow-x-hidden text-neutral-300 antialiased selection: bg-cyan-300 selection:text-cyan-900'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,12 @@ import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 
-
-
 const Navbar = () => {
     return (
-        <nav className="mb-20 flex items-center justify-between py-6">
+        <nav className="mb-20 flex items-center justify-between py-6 flex-row w-[100%]">
 
-            <div className='flex flex-shrink-0 items-center '>
-                <img className="mx-2 w-10" src={logo} alt="logo" />
+            <div className='flex w-[30px] h-auto'>
+                <img className="mx-2 w-[30px] h-auto" src={logo} alt="logo" />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
                 <FaLinkedin />
